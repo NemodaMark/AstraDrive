add82e5725314eea4647af0d616be9d205f05c5b
HaLTeX1 <pracni@gmail.com>
2023-10-12 01:20:23 +0200

    Update email.blade.php

--- elsobeadando-app/resources/views/vendor/notifications/email.blade.php
+++ elsobeadando-app/resources/views/vendor/notifications/email.blade.php
@@ -47,8 +47,8 @@
 @isset($actionText)
 <x-slot:subcopy>
 @lang(
-    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
+    "Amennyiben problémáid vannak a \":actionText\" gombbal, másold ki és illeszd be a fenti keresősávba az alábbi linket\n".
+    'a webböngésződben:',
     [
         'actionText' => $actionText,
     ]
