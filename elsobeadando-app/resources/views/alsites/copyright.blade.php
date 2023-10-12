ee21c18917a8787e8470a14e25fc9f9a1edffef4
HaLTeX1 <pracni@gmail.com>
2023-10-12 23:54:37 +0200

    Copyright 1.0
    - Copyright kártya kialakítva
    - Copyright linkek hozzáadva (még bővülhet)

--- elsobeadando-app/resources/views/alsites/copyright.blade.php
+++ elsobeadando-app/resources/views/alsites/copyright.blade.php
@@ -2,7 +2,18 @@
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-8">
-
+        <div class="card">
+            <div class="card-header">{{ __('Felhasznált tartalmak') }}</div>
+            <div class="card-body">
+                <h4>Hang-és videóanyag</h4>
+                <a href="https://www.youtube.com/watch?v=V3jd5Ob0KBI">GM - Opel Astra F</a> - Szerző : <a href="https://www.youtube.com/watch?v=V3jd5Ob0KBI">Vajda Balázs</a> <br>
+                <a href="https://www.youtube.com/watch?v=g-ypPq5wxa4">Opel Astra Kupa Rábaring 2002</a> - Szerző : <a href="https://www.youtube.com/@autosportfan7765">Autosportfan</a> <br>
+                <a href="https://www.youtube.com/watch?v=3qJdC78QPNc">Arno Opel Astra F GSI 2.0 16V | Cinematic Video</a> - Szerző : <a href="https://www.youtube.com/@YOKAMEDIA">YOKAMEDIA</a> <br>
+                <a href="https://onedrive.live.com/embed?resid=4ABE77F844B529FD%2160416&amp;authkey=!ANR4SIuNq_LllSQ&amp;em=2&amp;wdAr=1.7777777777777777">Astra Interaktív tananyag</a> - Szerző : <a href="https://github.com/NemodaMark/opel">Réz Levente László & Nemoda Márk Levente</a>
+                <hr>
+                <h4>Szakirodalom</h4>
+                <a href="https://hu.wikipedia.org/wiki/Opel_Astra_F">Opel Astra F </a> - Szerző : <a href="https://hu.wikipedia.org/wiki/Kezd%C5%91lap">Wikipedia</a> <br>
+        </div>
     </div>
 </div>
 @endsection
