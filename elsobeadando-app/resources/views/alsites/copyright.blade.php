8a487ecb1faa10402b93a5735c715d57c1ff2283
Réz Levente <pracni@gmail.com>
2023-11-06 23:11:57 +0100

    Copyright view update
    - Copyright linkek másik blankpage-ben nyílnak meg

--- elsobeadando-app/resources/views/alsites/copyright.blade.php
+++ elsobeadando-app/resources/views/alsites/copyright.blade.php
@@ -6,15 +6,15 @@
             <div class="card-header">{{ __('Felhasznált tartalmak') }}</div>
             <div class="card-body">
                 <h4>Hang-és videóanyag</h4>
-                <a href="https://www.youtube.com/watch?v=V3jd5Ob0KBI">GM - Opel Astra F</a> - Szerző : <a href="https://www.youtube.com/watch?v=V3jd5Ob0KBI">Vajda Balázs</a> <br>
-                <a href="https://www.youtube.com/watch?v=g-ypPq5wxa4">Opel Astra Kupa Rábaring 2002</a> - Szerző : <a href="https://www.youtube.com/@autosportfan7765">Autosportfan</a> <br>
-                <a href="https://www.youtube.com/watch?v=3qJdC78QPNc">Arno Opel Astra F GSI 2.0 16V | Cinematic Video</a> - Szerző : <a href="https://www.youtube.com/@YOKAMEDIA">YOKAMEDIA</a> <br>
-                <a href="https://www.youtube.com/watch?v=vvGJZKavU0o">Opel Astra F gyártása Magyarországon</a> - Szerző : <a href="https://www.youtube.com/@yeahsplungeformetoo">yeahsplungeformetoo</a> <br>
-                <a href="https://onedrive.live.com/embed?resid=4ABE77F844B529FD%2160416&amp;authkey=!ANR4SIuNq_LllSQ&amp;em=2&amp;wdAr=1.7777777777777777">Astra Interaktív tananyag</a> - Szerző : <a href="https://github.com/NemodaMark/opel">Réz Levente László & Nemoda Márk Levente</a>
+                <a href="https://www.youtube.com/watch?v=V3jd5Ob0KBI" target="_Blank">GM - Opel Astra F</a> - Szerző : <a href="https://www.youtube.com/watch?v=V3jd5Ob0KBI" target="_Blank">Vajda Balázs</a> <br>
+                <a href="https://www.youtube.com/watch?v=g-ypPq5wxa4" target="_Blank">Opel Astra Kupa Rábaring 2002</a> - Szerző : <a href="https://www.youtube.com/@autosportfan7765" target="_Blank">Autosportfan</a> <br>
+                <a href="https://www.youtube.com/watch?v=3qJdC78QPNc" target="_Blank">Arno Opel Astra F GSI 2.0 16V | Cinematic Video</a> - Szerző : <a href="https://www.youtube.com/@YOKAMEDIA" target="_Blank">YOKAMEDIA</a> <br>
+                <a href="https://www.youtube.com/watch?v=vvGJZKavU0o" target="_Blank">Opel Astra F gyártása Magyarországon</a> - Szerző : <a href="https://www.youtube.com/@yeahsplungeformetoo" target="_Blank">yeahsplungeformetoo</a> <br>
+                <a href="https://onedrive.live.com/embed?resid=4ABE77F844B529FD%2160416&amp;authkey=!ANR4SIuNq_LllSQ&amp;em=2&amp;wdAr=1.7777777777777777" target="_Blank">Astra Interaktív tananyag</a> - Szerző : <a href="https://github.com/NemodaMark/opel" target="_Blank">Réz Levente László & Nemoda Márk Levente</a>
                 
                 <hr>
                 <h4>Szakirodalom</h4>
-                <a href="https://hu.wikipedia.org/wiki/Opel_Astra_F">Opel Astra F </a> - Szerző : <a href="https://hu.wikipedia.org/wiki/Kezd%C5%91lap">Wikipedia</a> <br>
+                <a href="https://hu.wikipedia.org/wiki/Opel_Astra_F" target="_Blank">Opel Astra F </a> - Szerző : <a href="https://hu.wikipedia.org/wiki/Kezd%C5%91lap" target="_Blank">Wikipedia</a> <br>
         </div>
     </div>
 </div>
