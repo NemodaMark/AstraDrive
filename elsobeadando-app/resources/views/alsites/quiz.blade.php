e1e853200d47ead30c3fa30fff87bb421b75d33e
Réz Levente <pracni@gmail.com>
2023-11-27 01:09:34 +0100

    Update quiz.blade.php

--- elsobeadando-app/resources/views/alsites/quiz.blade.php
+++ elsobeadando-app/resources/views/alsites/quiz.blade.php
@@ -5,10 +5,23 @@
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-8">
+        <style>
+            .question-divider {
+                margin-top: 20px;
+                margin-bottom: 20px;
+                border: 1px solid #ccc;
+            }
+        </style>
+
         <form action="{{ route('quiz.submit') }}" method="post">
             @csrf
 
-            @foreach ($questions as $question)
+            @foreach ($questions as $key => $question)
+                @if($key > 0)
+                    <!-- Vízszintes vonal -->
+                    <hr class="question-divider">
+                @endif
+
                 <p>{{ $question->question }}</p>
 
                 @foreach ($question->answers as $answer)
@@ -23,7 +36,6 @@
                 <button type="submit" class="btn btn-warning">Válaszok beküldése</button>
             </div>
         </form>
-
         @if(session('score'))
         <div class="mt-3" style="background-color: #d4edda; padding: 10px; border-radius: 5px; display: inline-block;">
                 <p>Elért pontszám: <span class="score">8/{{ session('score') }}</span> <i class="fas fa-check"></i></p>
