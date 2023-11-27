425c8b16f3fe7337dc9fa1196c602134317c4639
HaLTeX1 <pracni@gmail.com>
2023-11-27 20:01:52 +0100

    Homecontroller upd

--- elsobeadando-app/app/Http/Controllers/Auth/LoginController.php
+++ elsobeadando-app/app/Http/Controllers/Auth/LoginController.php
@@ -19,8 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
-
+    use AuthenticatesUsers; 
     /**
      * Where to redirect users after login.
      *
--- elsobeadando-app/app/Http/Controllers/HomeController.php
+++ elsobeadando-app/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
@@ -35,6 +36,40 @@ class HomeController extends Controller
     public function edit()
     {
         return view('alsites.edit');
+    }
+    public function pictureUpload(Request $request){
+        if (session() -> has('user')){
+            $data = $request->validate(
+                [
+                    'image' => 'required|image|max:1024'
+                ],
+                [
+                    'image.required' => 'Kép feltöltése kötelező!',
+                    'image.image' => 'Amit feltöltöttél nem kép fájl!',
+                    'image.max' => 'A kép maximum 1 MB lehet!'
+                ]
+                );
+            foreach (session()->get('user') as $user)
+                $userid = $user->user_id;
+
+            $eredetikep = DB::select("SELECT user_profilepic FROM users WHERE user_id = '$userid'");
+            foreach ($eredetikep as $eredet) {
+                if ($eredet->user_profilepic != "defaultProfilePic.png") {
+                    $deleted=Storage::disk('public')->delete(public_path('profilepics/'.$eredet->user_profilepic));
+                }
+            }
+            $imgstring = $user->user_username.$request->image->getClientOriginalName();
+
+            DB::table('users')->where('user_id','=', $userid)->update(['user_profilepic' => $imgstring]);
+            $user->user_profilepic = $imgstring;
+
+            $request->image->move(public_path('profilepics'), $imgstring);
+
+            return back()->with('success', 'Kép sikeresen elmentve');
+        }
+        else{
+            return to_route('register');
+        }
     }
 
 }
